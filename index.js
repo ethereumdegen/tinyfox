c3c39df1890ab763c8d6040393f75189ea8074d6
andy <admazzola@gmail.com>
2021-03-27 12:31:05 -0400

    suspicious logging

--- index.js
+++ index.js
@@ -189,7 +189,10 @@ module.exports =  class TinyFox {
                 console.log('saved event data ', results.startBlock, ":", results.endBlock, ' Count: ' , results.events.length)
             }
 
-          //  console.log('results', results)
+            if(results.events.length == 0){
+                console.log('zero results', results)
+            }
+            
 
             if(results.events.length > SAFE_EVENT_COUNT  ){
                     stepSizeScaleFactor  = parseInt(stepSizeScaleFactor * 2)
